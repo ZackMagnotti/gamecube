a27ae2103e60f4531cbe077963d6953c3fc66692
zacbook <zack@magnotti.net>
2020-11-13 13:00:35 -0600

    fixes

--- src/swap.sh
+++ src/swap.sh
@@ -1,1 +1,10 @@
-echo 'hello world'
+
+active="/home/zack/gctest/games/active"
+all="/home/zack/gctest/games/all"
+favorites="/home/zack/gctest/games/favorites"
+nostalgia="/home/zack/gctest/games/nostalgia"
+temp="/home/zack/gctest/games/.temp"
+
+mv $all/* $temp
+mv $active/* $all
+mv $temp/* $active
